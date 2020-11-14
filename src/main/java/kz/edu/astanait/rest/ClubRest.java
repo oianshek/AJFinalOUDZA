1f8133d571112159868dcff2b399848021118a93
Ulan <ulan.buzhbanov620@gmail.com>
2020-11-14 21:34:30 +0600

    Servlets

---
+++ src/main/java/kz/edu/astanait/rest/ClubRest.java
@@ -0,0 +1,83 @@
+package kz.edu.astanait.rest;
+
+import kz.edu.astanait.controllers.ClubController;
+import kz.edu.astanait.controllers.EventController;
+import kz.edu.astanait.models.Club;
+import kz.edu.astanait.models.Event;
+import kz.edu.astanait.models.News;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+@Path("/club")
+public class ClubRest {
+    private final ClubController clubController = new ClubController();
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        List<Club> club;
+        try {
+            club = clubController.getAll();
+        } catch (BadRequestException e) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+        return Response
+                .ok(club)
+                .build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(Club club) {
+        try {
+            clubController.add(club);
+        } catch (BadRequestException e) {
+            return Response
+                    .notModified()
+                    .entity(e.getMessage())
+                    .build();
+        }
+        return Response
+                .ok("New club added successfully.")
+                .build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(Club club) {
+        try {
+            clubController.update(club);
+        } catch (BadRequestException e) {
+            return Response
+                    .notModified()
+                    .entity(e.getMessage())
+                    .build();
+        }
+        return Response
+                .ok("Club updated successfully.")
+                .build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response delete(@PathParam("id") int id) {
+        try{
+            clubController.delete(id);
+        } catch (BadRequestException e) {
+            return Response
+                    .notModified()
+                    .entity("Club hasn't deleted.")
+                    .build();
+        }
+        return Response
+                .ok("Club deleted successfully.")
+                .build();
+    }
+}
